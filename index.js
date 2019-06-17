4b6b54c7b94dc0dd19c17cf4344ccad98e0b5530
Sean Altemose <sean@seanaltemose.com>
2019-06-17 18:12:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function Dog(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
