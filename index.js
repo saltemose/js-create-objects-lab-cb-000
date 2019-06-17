2cb560f4f6cf8ca4a45a2ebadc0cca4a0bc28353
Sean Altemose <sean@seanaltemose.com>
2019-06-17 18:14:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function Cat(name, breed, age){
   this.breed = breed;
   this.age = age;
 }
+
+function Ferret(name, age, foodRequirements){
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+
+function Bird(name, breed){
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name, species, waterConditions){
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
